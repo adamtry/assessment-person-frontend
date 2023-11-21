cbed683e8d82d23d62ee3984ffafd9f33ec9a8fc
Adam <adam.tracy20@gmail.com>
2023-11-21 21:15:04 +0000

    More info message

--- apps/person/src/Components/NotFound.tsx
+++ apps/person/src/Components/NotFound.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
 export const NotFound = () => {
-  return <p className="lbh-body-s">Page not found</p>;
+  return <div>
+    <p className="lbh-body-s">Page not found</p>
+    <p className="lbh-body-s">Is the API running at {process.env.API_URL}?</p>
+    </div>;
 };
